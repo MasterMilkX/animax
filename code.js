9a8a313185b2896eb3a0b058befabd0e18e1ba44
Milk Charity <mastermilkx@gmail.com>
2018-02-23 13:13:00 -0500

    Version 0.1 - Base layout

---
+++ code.js
@@ -0,0 +1,13 @@
+// SOFTWARE CODE
+// by Milk
+
+//set up the canvas
+var canvas = document.getElementById("spriteWindow");
+var ctx = canvas.getContext("2d");
+
+// directionals
+var upKey = 38;     //[Up]
+var leftKey = 37;   //[Left]
+var rightKey = 39;  //[Rigt]
+var downKey = 40;   //[Down]
+var moveKeySet = [upKey, leftKey, rightKey, downKey];
